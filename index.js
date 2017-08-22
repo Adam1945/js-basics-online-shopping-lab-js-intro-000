8285570e461367cc6abb186c2fa384980dcc7061
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 23:41:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function viewCart() {
     console.log("Your shopping cart is empty.");
   }
   else {
-    var bag = [];
-    for(var i = 0; i < cart.length; i++) {
-      for(var item in cart[i]) {
-        bag.push(item + " at $" + cart[i][item])
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
       }
     }
-    console.log("In your cart, you have " + bag.join(", ") + ".");
+    console.log("In your cart, you have " + items.join(", ") + ".");
   }
 }
 
