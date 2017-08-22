5d2f0256ffad1efc3b878479940aa0c5fc7d9b94
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 23:15:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 function viewCart() {
   var i = 0;
   var bag = [];
-  if(cart == 0) {
+  if(cart.length == 0) {
     return "Your shopping cart is empty."
   }
   else {
