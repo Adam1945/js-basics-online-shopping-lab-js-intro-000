e6642ce203edfa9e08f8ed8a0a060569bdb44cf2
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 23:47:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var price = Math.floor((Math.random() * 100) + 1);
- var itemObject = {}
- itemObject[item] = price;
- cart.push(itemObject)
+ cart.push(new Object({[item]:price}))
  console.log(`${item} has been added to your cart.`)
  return cart;
 }
